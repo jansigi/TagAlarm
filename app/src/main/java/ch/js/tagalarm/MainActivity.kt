571b8ef315c81bf7635e955c2327f83128472e84
Jan Sigrist <jan.sigrist@ergon.ch>
2025-02-28 10:49:40 +0100

    request for alarm permission if needed

--- app/src/main/java/ch/js/tagalarm/MainActivity.kt
+++ app/src/main/java/ch/js/tagalarm/MainActivity.kt
@@ -1,5 +1,10 @@
 package ch.js.tagalarm
 
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Vibrator
 import androidx.activity.ComponentActivity
@@ -16,11 +21,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        requestExactAlarmPermissionIfNeeded()
         setContent {
             MaterialTheme {
                 Surface {
                     TagAlarmNavHost(vibrator = vibrator)
                 }
+            }
+        }
+    }
+
+    private fun requestExactAlarmPermissionIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            if (!alarmManager.canScheduleExactAlarms()) {
+                val actionRequestScheduleExactAlarm = "android.settings.REQUEST_SCHEDULE_EXACT_ALARM"
+                val intent = Intent(actionRequestScheduleExactAlarm).apply {
+                    data = Uri.parse("package:$packageName")
+                }
+                startActivity(intent)
             }
         }
     }
